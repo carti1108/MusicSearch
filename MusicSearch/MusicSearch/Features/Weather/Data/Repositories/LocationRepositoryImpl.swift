c647d50029cdf7d053ee9aa2369086ebe3f39517
kiseok <carti1108@naver.com>
2025-12-05 23:31:28 +0900

    feat: LocationRepository 구현체 구현
    - CLLocation 관련 프로토콜로 추상화
    - timeout 기능 추가

---
+++ MusicSearch/MusicSearch/Features/Weather/Data/Repositories/LocationRepositoryImpl.swift
@@ -0,0 +1,92 @@
+//
+//  LocationRepositoryImpl.swift
+//  MusicSearch
+//
+//  Created by Kiseok on 12/5/25.
+//
+
+import Foundation
+import CoreLocation
+
+protocol LocationManaging {
+	var authorizationStatus: CLAuthorizationStatus { get }
+	var desiredAccuracy: CLLocationAccuracy { get set }
+	func requestWhenInUseAuthorization()
+	func requestLocation() async throws -> CLLocation
+}
+
+extension CLLocationManager: LocationManaging {
+	func requestLocation() async throws -> CLLocation {
+		for try await update in CLLocationUpdate.liveUpdates() {
+			if let location = update.location {
+				return location
+			}
+		}
+		throw WeatherError.locationFetchFailed
+	}
+}
+
+final class LocationRepositoryImpl: LocationRepository {
+	
+	private var locationManager: LocationManaging
+	private let timeout: TimeInterval
+	
+	init(
+		locationManager: LocationManaging = CLLocationManager(),
+		timeout: TimeInterval = 10.0
+	) {
+		self.locationManager = locationManager
+		self.locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+		self.timeout = timeout
+	}
+	
+	func fetchCurrentLocation() async throws -> (latitude: Double, longitude: Double) {
+		let status = locationManager.authorizationStatus
+		
+		switch status {
+		case .denied, .restricted:
+			throw WeatherError.locationPermissionDenied
+			
+		case .notDetermined:
+			locationManager.requestWhenInUseAuthorization()
+			
+		case .authorizedWhenInUse, .authorizedAlways:
+			break
+			
+		@unknown default:
+			throw WeatherError.locationPermissionDenied
+		}
+		
+		let location = try await withTimeout(timeout) { [weak self] in
+			guard let self = self else { throw WeatherError.unknown }
+			return try await self.locationManager.requestLocation()
+		}
+		
+		return (location.coordinate.latitude, location.coordinate.longitude)
+	}
+	
+	private func withTimeout<T>(
+		_ timeout: TimeInterval,
+		perform task: @escaping () async throws -> T
+	) async throws -> T {
+		try await withThrowingTaskGroup(of: T.self) { group in
+			group.addTask {
+				try await task()
+			}
+			
+			group.addTask {
+				try await Task.sleep(for: .seconds(timeout))
+				throw WeatherError.locationFetchFailed
+			}
+			
+			guard let result = try await group.next() else {
+				group.cancelAll()
+				throw WeatherError.unknown
+			}
+
+			group.cancelAll()
+
+			return result
+		}
+	}
+}
