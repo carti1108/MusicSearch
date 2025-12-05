c1713c2bfd41a91f3e5c16f69122af06bd89a6ae
kiseok <carti1108@naver.com>
2025-12-05 23:33:30 +0900

    feat: FeatchCurrentWeatherUseCase 구현체 구현
    - 내부에서 Location, Weather Repository 조합해서 Weather 모델 받아오기

---
+++ MusicSearch/MusicSearch/Features/Weather/Domain/UseCases/FetchCurrentWeatherUseCaseImpl.swift
@@ -0,0 +1,29 @@
+//
+//  FetchCurrentWeatherUseCaseImpl.swift
+//  MusicSearch
+//
+//  Created by Kiseok on 12/5/25.
+//
+
+import Foundation
+
+struct FetchCurrentWeatherUseCaseImpl: FetchCurrentWeatherUseCase {
+
+	private let locationRepository: LocationRepository
+	private let weatherRepository: WeatherRepository
+
+	init(locationRepository: LocationRepository, weatherRepository: WeatherRepository) {
+		self.locationRepository = locationRepository
+		self.weatherRepository = weatherRepository
+	}
+
+	func execute() async throws -> Weather {
+		let (latitude, longitude) = try await self.locationRepository.fetchCurrentLocation()
+		let weather = try await self.weatherRepository.fetchCurrentWeather(
+			latitude: latitude,
+			longitude: longitude
+		)
+
+		return weather
+	}
+}
