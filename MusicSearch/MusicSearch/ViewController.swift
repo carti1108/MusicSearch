a12473acdea2f9a22e3e9049bd6589c1ec1ddc92
kiseok <carti1108@naver.com>
2025-12-05 19:36:10 +0900

    chore: 파일 생성

---
+++ MusicSearch/MusicSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicSearch
+//
+//  Created by Kiseok on 12/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ MusicSearch/MusicSearchTests/MusicSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSearchTests.swift
+//  MusicSearchTests
+//
+//  Created by Kiseok on 12/5/25.
+//
+
+import Testing
+@testable import MusicSearch
+
+struct MusicSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
