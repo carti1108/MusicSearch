d675163319b817fc1f0b7b74af557734d8be8436
kiseok <carti1108@naver.com>
2025-12-09 13:12:24 +0900

    feat: WeatherMusicCuration 모델 정의

---
+++ MusicSearch/MusicSearch/Features/Home/Domain/Entities/WeatherMusicCuration.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherMusicCuration.swift
+//  MusicSearch
+//
+//  Created by Kiseok on 12/7/25.
+//
+
+import Foundation
+
+public struct WeatherMusicCuration {
+	let weather: Weather
+	let moodTag: String
+	let tracks: [Track]
+}
