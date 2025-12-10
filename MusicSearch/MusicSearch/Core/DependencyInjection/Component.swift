2acdad33187a91ebdcd2e87616c01154a54ad50d
kiseok <carti1108@naver.com>
2025-12-10 13:33:31 +0900

    feat: Dependency, Component 프로토콜 정의

---
+++ MusicSearch/MusicSearch/Core/DependencyInjection/Component.swift
@@ -0,0 +1,12 @@
+//
+//  Component.swift
+//  MusicSearch
+//
+//  Created by Kiseok on 12/10/25.
+//
+
+protocol Component {
+	associatedtype DependencyType: Dependency
+	
+	init(dependency: DependencyType)
+}
---
+++ MusicSearch/MusicSearch/Core/DependencyInjection/Dependency.swift
@@ -0,0 +1,10 @@
+//
+//  Dependency.swift
+//  MusicSearch
+//
+//  Created by Kiseok on 12/9/25.
+//
+
+
+protocol Dependency { }
+
