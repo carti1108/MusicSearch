a7869159886c0089467f1df12051c69e0b7e2357
kiseok <carti1108@naver.com>
2026-07-17 17:34:04 +0900

    test: SpotifyAppService 네트워크 호출 및 예외 상황 테스트 추가

--- MusicSearch/Core/Data/Tests/Sources/SpotifyAppServiceTests.swift
+++ MusicSearch/Core/Data/Tests/Sources/SpotifyAppServiceTests.swift
@@ -6,12 +6,16 @@ import NetworkLayer
 
 struct MockNetworkManager: NetworkRequesting {
     var errorToThrow: Error?
+    var responseToReturn: Any?
     
     func perform<T: Decodable, E: Requestable>(with endpoint: E, as type: T.Type) async throws -> T {
         if let error = errorToThrow {
             throw error
         }
-        fatalError("Not implemented for success case in this mock")
+        if let response = responseToReturn as? T {
+            return response
+        }
+        fatalError("Not implemented for this type")
     }
 }
 
@@ -69,5 +73,88 @@ struct SpotifyAppServiceTests {
         #expect(fallbackURL != nil)
         #expect(fallbackURL?.absoluteString.contains("search/Hysteria%20Muse") == true)
         #expect(fallbackURL?.host == "open.spotify.com")
+    }
+    
+    @Test("정상 응답 시 트랙 딥링크를 성공적으로 반환하는가")
+    func testFetchTrackDeepLinkSuccess() async throws {
+        // Given
+        let mockDTO = SpotifyTrackDTO(
+            id: "1", name: "Hysteria", uri: "spotify:track:1",
+            artists: [SpotifyArtistDTO(id: "2", name: "Muse", uri: nil, external_urls: nil)],
+            album: nil,
+            external_urls: SpotifyExternalURLs(spotify: "https://open.spotify.com/track/1")
+        )
+        let response = SpotifyTrackSearchResponse(tracks: SpotifyItems(items: [mockDTO], total: 1))
+        let mockNetwork = MockNetworkManager(responseToReturn: response)
+        let mockAuth = MockMusicAuthService()
+        let repository = SpotifyAppService(networkManager: mockNetwork, authRepository: mockAuth)
+        
+        let track = Track(title: "Hysteria", artist: "Muse", imageURL: nil)
+        
+        // When
+        let url = await repository.fetchDeepLink(for: track)
+        
+        // Then
+        #expect(url?.absoluteString == "https://open.spotify.com/track/1")
+    }
+    
+    @Test("정상 응답 시 아티스트 딥링크를 성공적으로 반환하는가")
+    func testFetchArtistDeepLinkSuccess() async throws {
+        // Given
+        let mockDTO = SpotifyArtistDTO(
+            id: "2", name: "Muse", uri: "spotify:artist:2",
+            external_urls: SpotifyExternalURLs(spotify: "https://open.spotify.com/artist/2")
+        )
+        let response = SpotifyArtistSearchResponse(artists: SpotifyItems(items: [mockDTO], total: 1))
+        let mockNetwork = MockNetworkManager(responseToReturn: response)
+        let mockAuth = MockMusicAuthService()
+        let repository = SpotifyAppService(networkManager: mockNetwork, authRepository: mockAuth)
+        
+        // When
+        let url = await repository.fetchDeepLink(for: "Muse")
+        
+        // Then
+        #expect(url?.absoluteString == "https://open.spotify.com/artist/2")
+    }
+    
+    @Test("external_urls가 없을 경우 uri를 기반으로 Web URL을 생성하는가")
+    func testFallbackToURIConversion() async throws {
+        // Given
+        let mockDTO = SpotifyArtistDTO(
+            id: "2", name: "Muse", uri: "spotify:artist:2",
+            external_urls: nil
+        )
+        let response = SpotifyArtistSearchResponse(artists: SpotifyItems(items: [mockDTO], total: 1))
+        let mockNetwork = MockNetworkManager(responseToReturn: response)
+        let mockAuth = MockMusicAuthService()
+        let repository = SpotifyAppService(networkManager: mockNetwork, authRepository: mockAuth)
+        
+        // When
+        let url = await repository.fetchDeepLink(for: "Muse")
+        
+        // Then
+        #expect(url?.absoluteString == "https://open.spotify.com/artist/2")
+    }
+    
+    @Test("Spotify Tracks 검색을 성공적으로 수행하는가")
+    func testSearchSpotifyTracks() async throws {
+        // Given
+        let mockDTO = SpotifyTrackDTO(
+            id: "1", name: "Hysteria", uri: "spotify:track:1",
+            artists: [SpotifyArtistDTO(id: "2", name: "Muse", uri: nil, external_urls: nil)],
+            album: nil, external_urls: nil
+        )
+        let response = SpotifyTrackSearchResponse(tracks: SpotifyItems(items: [mockDTO], total: 100))
+        let mockNetwork = MockNetworkManager(responseToReturn: response)
+        let mockAuth = MockMusicAuthService()
+        let repository = SpotifyAppService(networkManager: mockNetwork, authRepository: mockAuth)
+        
+        // When
+        let result = try await repository.searchSpotifyTracks(query: "Hysteria", limit: 20, offset: 0)
+        
+        // Then
+        #expect(result.totalResults == 100)
+        #expect(result.tracks.count == 1)
+        #expect(result.tracks.first?.title == "Hysteria")
     }
 }
