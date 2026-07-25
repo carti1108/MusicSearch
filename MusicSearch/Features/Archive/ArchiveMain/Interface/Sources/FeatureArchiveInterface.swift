7a5efe9817d124ade9c9b89de566c3edd75a3cb9
kiseok <carti1108@naver.com>
2026-07-25 22:14:52 +0900

    refactor: Archive 하위 모듈 Interface 타겟 내의 불필요한 TCA State 및 Action 선언부 일괄 제거

--- MusicSearch/Features/Archive/ArchiveMain/Interface/Sources/FeatureArchiveInterface.swift
+++ MusicSearch/Features/Archive/ArchiveMain/Interface/Sources/FeatureArchiveInterface.swift
@@ -1,3 +1,25 @@
-// FeatureArchiveInterface.swift
-// ArchiveState and ArchiveAction have been moved to ArchiveMain/Sources/TCA/ArchiveFeature.swift
-// to fully support @Reducer enum for Path and Destination without circular module dependencies.
+import Foundation
+import UIKit
+import MicroRIBs
+import ArchiveDomain
+import MSDomain
+import TrackSearchDomain
+
+@MainActor
+public protocol ArchiveDependency: MicroRIBs.Dependency {
+    var archiveRepository: any ArchiveRepository { get }
+    var searchTracksUseCase: any SearchTracksUseCase { get }
+    var exportPlaylistUseCase: any ExportPlaylistUseCase { get }
+    var getMusicAccessTokenUseCase: any GetMusicAccessTokenUseCase { get }
+    var authorizeMusicUseCase: any AuthorizeMusicUseCase { get }
+}
+
+public protocol ArchiveBuildable: Buildable {
+    func build(withListener listener: ArchiveListener) -> ArchiveRouting
+}
+
+public protocol ArchiveRouting: ViewableRouting {
+}
+
+public protocol ArchiveListener: AnyObject {
+}
